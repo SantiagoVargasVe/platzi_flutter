4b7c555827b140ff1b6ded370e66d824cf04c4f7
Santiago Vargas <santiago.vargas@imagineapps.co>
2021-12-30 22:36:03 -0500

    ADD: Añadidos parametros a widget de descripción

--- lib/description_place.dart
+++ lib/description_place.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 
-class DescriptionPlace extends StatelessWidget {
-  const DescriptionPlace({Key? key}) : super(key: key);
+class DescriptionPlace  extends StatelessWidget {
+
+  String namePlace;
+  int stars;
+  String descriptionPlace;
+
+  DescriptionPlace(this.namePlace, this.stars, this.descriptionPlace);
+
 
   @override
   Widget build(BuildContext context) {
@@ -10,18 +16,29 @@ class DescriptionPlace extends StatelessWidget {
       color: Color(0xFFf2C611),
     );
 
+    const starHalf = Icon(
+      Icons.star_half,
+      color: Color(0xFFf2C611),
+    );
+
+
+    const starBorder = Icon(
+      Icons.star_border,
+      color: Color(0xFFf2C611),
+    );
+
     const description = Text(
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sapien tortor, mollis a felis quis, congue molestie magna. Donec sed commodo sapien. Proin sodales nulla at eros iaculis, et vestibulum velit ornare. Aenean mollis justo sem, et vulputate velit auctor in. Nullam ullamcorper, dolor non dignissim volutpat, neque ligula vestibulum neque, sit amet dictum nibh neque quis eros. Cras vel sapien sed urna eleifend accumsan. Nunc non purus at mauris fermentum fermentum venenatis sed leo. Proin nec mauris suscipit, mattis eros id, aliquet lorem. Nunc non augue nulla. Mauris ullamcorper, dui sit amet egestas aliquam, augue tellus molestie sem, in tempus velit tortor ultricies ipsum. Vivamus lobortis ligula nec arcu pretium pellentesque. ",
       style: TextStyle(
-        fontSize: 12.0,
+        fontSize: 20.0,
         color: Color(0xFF56575a),
       ),
     );
 
-    final titleStars = Wrap(
-      children: <Widget> [
-        const Text(
-          "Duwili Ellaasdasda ",
+    final titleStars = Row(
+      children:  <Widget>[
+        const  Expanded(child: Text(
+          "Duwili Ella",
           overflow: TextOverflow.ellipsis,
           maxLines: 2,
           softWrap: false,
@@ -29,9 +46,14 @@ class DescriptionPlace extends StatelessWidget {
             fontSize: 30.0,
             fontWeight: FontWeight.w900,
             color: Color(0xFF56575a),
-          ),
+          )
+          ,
         ),
-       Row(
+        flex: 1,)
+        ,
+        Expanded(
+        flex: 0,
+        child: Row(
           children: const <Widget>[
             star,
             star,
@@ -40,11 +62,14 @@ class DescriptionPlace extends StatelessWidget {
             star,
           ],
         ),
+        ),
+
         
       ],
-    ) ; 
+    );
 
-    return Container(
+    return SingleChildScrollView(
+      child: Container(
       child: Column(
         children: <Widget>[
           titleStars,
@@ -52,7 +77,8 @@ class DescriptionPlace extends StatelessWidget {
           description,
         ],
       ),
-      margin: const EdgeInsets.only(top: 320.0, left: 20.0, right: 20.0),
-    );
+      margin: const EdgeInsets.only(top: 320.0, left: 20.0, right: 20.0, bottom: 30.0),
+      
+    ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -49,18 +49,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -74,9 +62,9 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text("uwu"),
+        title: const Text("uwu"),
       ),
-      body: new DescriptionPlace()
+      body: DescriptionPlace( "Duwili Ella", 3 , "dasdfasdfasdfasdfasdf" )
     );
   }
 }
